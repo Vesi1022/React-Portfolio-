af846c61c7ea036ca294cb2f3ff53be82040a232
Vesi1022 <109995670+Vesi1022@users.noreply.github.com>
2023-02-13 23:21:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import './App.css';
+import about me  from './components/about;
+
+function App() {
+  return (
+    <div className="header">
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
